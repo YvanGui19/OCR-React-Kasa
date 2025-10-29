f5e1e9766146c880c9503c52455daac765b113ea
Dixneuf Code <dixneufcode@gmail.com>
2025-10-29 11:49:03 +0100

    initialisation dependancies

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,48 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage', '.vite']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ['src/**/*.{js,jsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
+
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+
+    extends: [js.configs.recommended, eslintConfigPrettier],
+
     rules: {
+      // React
+      ...(react.configs.recommended?.rules ?? {}),
+
+      // Hooks
+      ...(reactHooks.configs['recommended-latest']?.rules ?? {}),
+
+      // React Refresh (vite)
+      ...(reactRefresh.configs.vite?.rules ?? {}),
+
+      // Tes règles perso
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
     },
+
+    // détection automatique de la version de React
+    settings: { react: { version: 'detect' } },
   },
 ])
